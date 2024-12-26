86943beb2cf796d12ecee07718db830a0dfd2916
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-12-26 21:15:36 +0100

    feat: move application to ts

---
+++ sample.js
@@ -0,0 +1,12 @@
+function addTwoNumbers(num1, num2) {
+  const res = num1 + num2;
+  return res;
+}
+
+let resOne = addTwoNumbers("wwwww", "23");
+let resTwo = addTwoNumbers(2, 5);
+let resThree = addTwoNumbers(["a", "b"], ["c", "d"]);
+
+console.log(resOne);
+console.log(resTwo);
+console.log(resThree);
--- src/app.ts
+++ src/app.ts
@@ -4,4 +4,10 @@
  * Object oriented javascript
  */
 
-console.log("Hello World, i'm here");
+function addTwoNumbers(num1: number, num2: number): number {
+  const res = num1 + num2;
+  return res;
+}
+
+let resTwo = addTwoNumbers(2, 5);
+console.log(resTwo);
